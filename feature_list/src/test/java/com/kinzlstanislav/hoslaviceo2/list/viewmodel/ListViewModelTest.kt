6d503df34e05927318e68111c678e7de52613799
Stanislav Kinzl <kinzlstanislav@gymnachod.cz>
2020-01-19 10:30:11 +0100

    ListViewModelTest

---
+++ feature_list/src/test/java/com/kinzlstanislav/hoslaviceo2/list/viewmodel/ListViewModelTest.kt
@@ -0,0 +1,69 @@
+package com.kinzlstanislav.hoslaviceo2.list.viewmodel
+
+import androidx.lifecycle.MutableLiveData
+import com.kinzlstanislav.hoslaviceo2.architecture.network.UsersRepository
+import com.kinzlstanislav.hoslaviceo2.architecture.network.UsersRepository.UsersFetchResult
+import com.kinzlstanislav.hoslaviceo2.architecture.network.UsersRepository.UsersFetchResult.*
+import com.kinzlstanislav.hoslaviceo2.architecture.network.model.User
+import com.kinzlstanislav.hoslaviceo2.base.extensions.set
+import com.kinzlstanislav.hoslaviceo2.list.viewmodel.ListViewModel.ListState
+import com.kinzlstanislav.hoslaviceo2.list.viewmodel.ListViewModel.ListState.*
+import com.kinzlstanislav.hoslaviceo2.unittesting.BaseViewModelTest
+import io.mockk.*
+import org.junit.Test
+
+class ListViewModelTest : BaseViewModelTest() {
+
+    private companion object {
+        val SOME_USER = User(0, "", "",  false, "")
+    }
+    private val mockUsersRepository = mockk<UsersRepository>()
+    private val mockState = mockk<MutableLiveData<ListState>>(relaxed = true)
+
+    private val subject = ListViewModel(mockUsersRepository, mockState)
+
+    @Test
+    fun `getUsers() - state set to UsersLoaded`() = getUsersTest(
+        resultRepositoryReturns = OnlineUsers(emptyList()),
+        desiredOutcomeState = UsersLoaded(emptyList())
+    )
+
+    @Test
+    fun `getUsers() - state set to UsersLoadedOffline`() = getUsersTest(
+        resultRepositoryReturns = OfflineUsers(emptyList()),
+        desiredOutcomeState = UsersLoadedOffline(emptyList())
+    )
+
+    @Test
+    fun `getUsers() - state set to  NetworkError`() = getUsersTest(
+        resultRepositoryReturns = NoSavedUsersNetworkError,
+        desiredOutcomeState = NetworkError
+    )
+
+    @Test
+    fun `getUsers() - state set to GenericError`() = getUsersTest(
+        resultRepositoryReturns = FetchUsersGenericError,
+        desiredOutcomeState = GenericError
+    )
+
+    @Test
+    fun `removeUserFromLocalDb() - removeUserFromLocalDb is called on repo`() {
+        subject.removeUserFromLocalDb(SOME_USER)
+        coVerify {
+            mockUsersRepository.removeUserFromLocalDb(SOME_USER)
+        }
+    }
+
+    private fun getUsersTest(
+        resultRepositoryReturns: UsersFetchResult,
+        desiredOutcomeState: ListState
+    ) {
+        coEvery { mockUsersRepository.fetchUsersOrGetLocal() } returns resultRepositoryReturns
+        subject.getUsers()
+        verifyOrder {
+            mockState.set(LoadingUsers)
+            mockState.set(desiredOutcomeState)
+        }
+    }
+
+}
